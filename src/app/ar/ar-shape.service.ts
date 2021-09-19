72e5485a4c8d081e4cb8d41637d7c07ca89ca471
BillMills <mills.wj@gmail.com>
2021-09-19 11:25:34 -0400

    typo

--- src/app/ar/ar-shape.service.ts
+++ src/app/ar/ar-shape.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { ARShape } from '../models/ar-shape'
-import { environment } from '../../../environments/environment';
+import { environment } from '../../environments/environment';
 import * as moment from 'moment';
 import { mockShapeComplex } from './ar-shape.parameters'
 @Injectable({
