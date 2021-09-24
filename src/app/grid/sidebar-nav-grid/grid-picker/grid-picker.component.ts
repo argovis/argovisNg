ed48cdd7fdc0e3fd3d705160d34061db0b943d54
Vivaswat Suresh <vivaswat.suresh@gmail.com>
2021-09-24 14:43:58 -0700

    fixed grid picker component file because it was bugged

--- src/app/grid/sidebar-nav-grid/grid-picker/grid-picker.component.ts
+++ src/app/grid/sidebar-nav-grid/grid-picker/grid-picker.component.ts
@@ -61,6 +61,7 @@ export class GridPickerComponent implements OnInit {
         this.availableGrids = this.selectGridService.getAvailableGrids(this.selectedProperty)
        }
       })
+  }
 
   getPropertyViewValue(param: string): string {
     const viewValue = this.availableProperties.find( (p) => p.param === param )['viewValue']
