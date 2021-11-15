16626724f0f25c5fee4b85dd6eb83601324aa472
billmills <you@example.com>
2021-11-15 12:24:07 -0500

    factor for convenient testing

--- src/app/profview/get-profiles.service.spec.ts
+++ src/app/profview/get-profiles.service.spec.ts
@@ -4,6 +4,8 @@ import { GetProfilesService } from './get-profiles.service';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { HttpClient, HttpErrorResponse, HttpClientModule, HttpHandler } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
+import { environment } from '../../environments/environment';
+
 describe('GetProfilesService', () => {
   let service: GetProfilesService;
 
@@ -22,4 +24,8 @@ describe('GetProfilesService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should handle queries with no BGC variables correctly', () => {
+    expect(service.construct_platform_query('1234', ['latitude', 'longitude', 'time'])).toEqual(environment.apiRoot + '/profiles?platforms=1234&bgcMeasurements=pres')
+  })
 });
