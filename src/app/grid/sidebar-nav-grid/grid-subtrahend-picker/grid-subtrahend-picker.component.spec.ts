10a02dae4f0c7818e103416c0fbcabee495dbb80
Xeanies <vivasuresh.projects@gmail.com>
2021-11-17 18:55:08 -0800

    Fixed up the spec test files for month subtrahend and grid subtrahend

--- src/app/grid/sidebar-nav-grid/grid-subtrahend-picker/grid-subtrahend-picker.component.spec.ts
+++ src/app/grid/sidebar-nav-grid/grid-subtrahend-picker/grid-subtrahend-picker.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GridSubtrahendPickerComponent } from './grid-subtrahend-picker.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { QueryGridService } from './../../query-grid.service';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClient, HttpErrorResponse, HttpClientModule, HttpHandler } from '@angular/common/http';
 
 describe('GridSubtrahendPickerComponent', () => {
   let component: GridSubtrahendPickerComponent;
@@ -8,7 +14,16 @@ describe('GridSubtrahendPickerComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ GridSubtrahendPickerComponent ]
+      declarations: [ GridSubtrahendPickerComponent ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+      providers: [
+        QueryGridService,
+        HttpClientTestingModule,
+        HttpTestingController,
+        HttpClient,
+        HttpClientModule,
+        HttpHandler, ],
+      imports: [ RouterTestingModule ]
     })
     .compileComponents();
   }));
